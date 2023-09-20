365303582a3e3f769dc65c45b56a72e55e203886
Hiago Martins de Sousa <hiagosousa77@gmail.com>
2023-09-20 16:23:13 -0300

    Routing

---
+++ chatSD/src/components/Login.tsx
@@ -0,0 +1,57 @@
+import { ChangeEvent, useState } from "react";
+import { BsChatDotsFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [login, setLogin] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = () => {
+    setLogin(login);
+    navigate("/home");
+  };
+
+  const handleLoginChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  };
+
+  return (
+    <div className="flex w-screen h-screen items-center justify-center bg-emerald-400">
+      <div className="flex h-3/4 w-2/6 min-w-[350px] bg-gray-600 flex-col justify-center items-center rounded-md">
+        <div className=" flex items-center justify-center h-1/5 w-1/5 flex-col">
+          <BsChatDotsFill className="text-white h-4/6 w-4/6" />
+          <p className="h-2/6 items-center justify-center text-white text-2xl">
+            Chat
+          </p>
+        </div>
+        <div className="flex flex-col justify-evenly items-center min-w-[250px] h-2/5 w-2/5 rounded-3xl bg-emerald-400 mb-20">
+          <div className="flex flex-col items-center space-y-5">
+            <p className="text-white text-2xl">Login</p>
+            <input
+              className="rounded-md p-3"
+              type="text"
+              placeholder="Digite seu Login"
+              value={login}
+              onChange={handleLoginChange}
+            />
+          </div>
+          <div>
+            <button
+              className="bg-orange-300 px-2 py-1 rounded-md"
+              onClick={handleLoginSubmit}
+            >
+              <p className="text-black">Entrar</p>
+            </button>
+          </div>
+        </div>
+        <div className="text-white grid text-center">
+          <p>Feito por: </p>
+          <p>Hiago Martins</p>
+          <p>Pedro Piva</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- chatSD/src/components/home.tsx
+++ chatSD/src/components/home.tsx
@@ -1,21 +1,18 @@
 import { FaUserCircle } from "react-icons/fa";
 import { CgLogOut } from "react-icons/cg";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function Home() {
   const { login } = useParams();
   const navigate = useNavigate();
   console.log(login);
 
-  const handleLogout = () => {
-    alert("opa");
-    navigate("/home");
-  };
+  const handleLogout = () => {};
 
   return (
     <div className="flex w-screen h-screen items-center justify-center bg-emerald-400">
-      <div className="relative flex h-[90%] w-3/5 min-w-[350px] bg-gray-600 rounded-md">
-        <div className="absolute top-0 bg-gray-400 border-2 rounded-t-md h-[5%] w-full flex flex-row items-center">
+      <div className=" flex-col h-[90%] w-3/5 min-w-[350px] bg-gray-600 rounded-md">
+        <div className=" top-0 bg-gray-400 border-2 rounded-t-md h-[10%] w-full flex flex-row items-center">
           <div className="text-white h-[60%] w-[40%]">
             <FaUserCircle className="text-white w-full h-full" />
           </div>
@@ -25,13 +22,15 @@ function Home() {
             </p>
           </div>
           <div className="flex border-l-2 h-full w-[4%] items-center justify-center">
-            <button className=" w-full h-full" onClick={handleLogout}>
+            <Link to="/" className=" w-full h-full" onClick={handleLogout}>
               <CgLogOut className="text-white items-center justify-center w-full h-full" />
-            </button>
+            </Link>
           </div>
         </div>
+        <div className="flex w-full py-5 pt-90 items-center justify-center">
+          <div className="bg-gray-400 flex  w-3/5 h-full">lul</div>
+        </div>
       </div>
-      Home
     </div>
   );
 }
